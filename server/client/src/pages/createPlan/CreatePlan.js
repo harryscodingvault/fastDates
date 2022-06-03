ef560f96e93519b60a66307c881946de190a6947
HaroldDRF <95robots@gmail.com>
2022-06-03 03:10:22 -0700

    create plan implemtnetd

--- server/client/src/pages/createPlan/CreatePlan.js
+++ server/client/src/pages/createPlan/CreatePlan.js
@@ -5,20 +5,65 @@ import "./CreatePlan.css";
 const initialState = {
   title: "",
   duration: "",
-  travel_time: "",
-  location: { city: "", state: "" },
-  destinations: [{ address: "" }],
+  location: "",
+  destination_1: "",
+  destination_2: "",
+  destination_3: "",
+  destination_4: "",
+  destination_5: "",
 };
 
 const CreatePlan = () => {
   const [values, setValues] = useState(initialState);
+  const [nDestinations, setNDestinations] = useState(1);
 
   const handleChange = (e) => {
-    console.log(e.target.value);
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
   };
+
   const onSubmit = (e) => {
+    const {
+      title,
+      duration,
+      location,
+      destination_1,
+      destination_2,
+      destination_3,
+      destination_4,
+      destination_5,
+    } = values;
+    const destinations = [
+      destination_1,
+      destination_2,
+      destination_3,
+      destination_4,
+      destination_5,
+    ];
+    const filteredD = destinations.filter((item) => {
+      return item !== "";
+    });
+    console.log("destinations", filteredD);
     e.preventDefault();
-    console.log(e.target.value);
+    console.log({ title, location, duration, destinations: filteredD });
+  };
+
+  const renderDestinationsInputs = () => {
+    let destinationsList = [];
+    for (let i = 1; i <= nDestinations; i++) {
+      destinationsList.push(
+        <FormInput
+          key={i}
+          type="text"
+          name={`destination_${i}`}
+          value={values[`destination_${i}`]}
+          handleChange={handleChange}
+          placeholder="Destination"
+        />
+      );
+    }
+    return destinationsList;
   };
 
   return (
@@ -39,34 +84,26 @@ const CreatePlan = () => {
           handleChange={handleChange}
           placeholder="Duration"
         />
-        <FormInput
-          type="number"
-          name="travel_time"
-          value={values.travel_time}
-          handleChange={handleChange}
-          placeholder="Travel Time"
-        />
+
         <FormInput
           type="text"
-          name="city"
-          value={values.location.city}
+          name="location"
+          value={values.location}
           handleChange={handleChange}
-          placeholder="City"
+          placeholder="Location"
         />
-        <FormInput
-          type="text"
-          name="state"
-          value={values.location.state}
-          handleChange={handleChange}
-          placeholder="State"
-        />
-        <FormInput
-          type="text"
-          name="destination"
-          value={values.destinations[0].address}
-          handleChange={handleChange}
-          placeholder="Destination"
-        />
+        <div className="create-plan-destinations-group">
+          {renderDestinationsInputs()}
+        </div>
+
+        {nDestinations !== 5 && (
+          <div
+            className="btn-add"
+            onClick={() => setNDestinations(nDestinations + 1)}
+          >
+            <h2>+</h2>
+          </div>
+        )}
 
         <button className="btn btn-block" type="submit">
           <h5>Submit</h5>
