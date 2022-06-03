38e4a8fedfe7d9b92ccf72364fa75c35c097caf4
HaroldDRF <95robots@gmail.com>
2022-06-02 20:36:21 -0700

    search plans form created

--- server/client/src/components/cardItemsList/CardItemsList.js
+++ server/client/src/components/cardItemsList/CardItemsList.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./CardItemsList.css";
+import { useDispatch, useSelector } from "react-redux";
 
 import CardItem from "../cardItem/CardItem";
 import { travels } from "../../data/data";
+import { getAllPlans } from "../../features//plans/planSlice";
 
 const CardItemsList = () => {
+  const { isLoading, error_message, plans } = useSelector(
+    (store) => store.plan
+  );
+
+  useEffect(() => {}, []);
+
   const renderList = travels.map((item) => (
     <CardItem data={item} key={item.id} />
   ));
