bf185036b8a2b70617a7d1df18a2aee1e09ff60a
HaroldDRF <95robots@gmail.com>
2022-06-07 00:47:15 -0700

    search plans style completed

--- server/client/src/pages/home/Home.js
+++ server/client/src/pages/home/Home.js
@@ -60,23 +60,25 @@ const Home = () => {
       )}
 
       <form className="home-search-form form" onSubmit={onSubmit}>
-        <div className="search-location">
-          <div className="search-label">
-            <h5>Search plan by</h5>
+        <div className="home-block">
+          <div className="search-location">
+            <div className="search-label">
+              <h5>Search plan by</h5>
+            </div>
+            <FormInput
+              type="location"
+              name="location"
+              values={location}
+              handleChange={handlePlanInput}
+              placeholder="City, State"
+            />
           </div>
-          <FormInput
-            type="location"
-            name="location"
-            values={location}
-            handleChange={handlePlanInput}
-            placeholder="City, State"
-          />
-        </div>
-        <div className="home-select-group">
+
           <div className="form-row form-row-select time-select">
             <div className="select-label">
               <h5>Created this</h5>
             </div>
+
             <select
               className="form-select"
               name="time"
@@ -93,47 +95,48 @@ const Home = () => {
               })}
             </select>
           </div>
-          <div className="duration-select-group">
-            <div className="select-label">
-              <h5>Hourly length from</h5>
-            </div>
-            <div className="form-row form-row-select">
-              <select
-                name="duration_1"
-                id="duration_1"
-                value={duration_1}
-                onChange={handlePlanInput}
-                className="form-select"
-              >
-                {durationOptions.map((itemValue, index) => {
-                  return (
-                    <option key={index} value={itemValue}>
-                      {itemValue}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-            <div className="select-label">
-              <h5>To</h5>
-            </div>
-            <div className="form-row form-row-select">
-              <select
-                name="duration_2"
-                id="duration_2"
-                value={duration_2}
-                onChange={handlePlanInput}
-                className="form-select"
-              >
-                {durationOptions.map((itemValue, index) => {
-                  return (
-                    <option key={index} value={itemValue}>
-                      {itemValue}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
+        </div>
+
+        <div className="duration-select-group">
+          <div className="select-label">
+            <h5>Hourly length from</h5>
+          </div>
+          <div className="form-row form-row-select">
+            <select
+              name="duration_1"
+              id="duration_1"
+              value={duration_1}
+              onChange={handlePlanInput}
+              className="form-select"
+            >
+              {durationOptions.map((itemValue, index) => {
+                return (
+                  <option key={index} value={itemValue}>
+                    {itemValue}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="select-label">
+            <h5>To</h5>
+          </div>
+          <div className="form-row form-row-select">
+            <select
+              name="duration_2"
+              id="duration_2"
+              value={duration_2}
+              onChange={handlePlanInput}
+              className="form-select"
+            >
+              {durationOptions.map((itemValue, index) => {
+                return (
+                  <option key={index} value={itemValue}>
+                    {itemValue}
+                  </option>
+                );
+              })}
+            </select>
           </div>
         </div>
 
